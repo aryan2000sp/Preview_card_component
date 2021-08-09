ced62e514a63f2a2a4e51f58cfa71b0d70f083b2
Aryan <aryan2000sp@gmail.com>
2021-08-09 05:03:14 +0400

    Completed the responsive layout of the webpage.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Responsive Webpage
+
+The main objective of this project was to
+build a reponsive webpage layout. This is one
+of the challenges on [Frontend Mentor](https://www.frontendmentor.io/challenges).
+
+---
+
+# Tools Used
+
+1. React jsx components
+2. SASS
+3. CSS
+4. HTML
+
+---
+
+# Learning Outcomes
+
+1. CSS flexbox property
+2. CSS media query
+3. SASS imports and nesting of elements.
+4. React jsx.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
-
+// import React from "react";
+import "./scss/main.scss";
+import sedans from "./images/icon-sedans.svg";
+import suvs from "./images/icon-suvs.svg";
+import luxury from "./images/icon-luxury.svg";
 function App() {
-  return <h1>Hey</h1>;
+  return (
+    <section className="main-section">
+      <article className="card">
+        <div className="col-1 col">
+          <img src={sedans} alt="" />
+          <h1>SEDANS</h1>
+          <p>
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="col-2 col">
+          <img src={suvs} alt="" />
+          <h1>SUVS</h1>
+          <p>
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="col-3 col">
+          <img src={luxury} alt="" />
+          <h1>LUXURY</h1>
+          <p>
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </p>
+          <button>Learn More</button>
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default App;
